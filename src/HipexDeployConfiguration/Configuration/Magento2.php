9f0656ea209c1e1b31c2ce02df0647b6579d4762
Freek Gruntjes <Fgruntjes@users.noreply.github.com>
2019-01-28 14:24:54 +0100

    Update Magento2.php

--- src/HipexDeployConfiguration/Configuration/Magento2.php
+++ src/HipexDeployConfiguration/Configuration/Magento2.php
@@ -37,7 +37,7 @@ class Magento2 extends Configuration
         $this->addBuildCommand(new Command\Build\Composer());
         $this->addBuildCommand(new Command\Build\Magento2\SetupDiCompile());
         $this->addBuildCommand(new Command\Build\Magento2\SetupStaticContentDeploy($localesFrontend, 'frontend'));
-        $this->addBuildCommand(new Command\Build\Magento2\SetupStaticContentDeploy($localesBackend, 'backend'));
+        $this->addBuildCommand(new Command\Build\Magento2\SetupStaticContentDeploy($localesBackend, 'adminhtml'));
 
         $this->addDeployCommand(new Command\Deploy\Magento2\MaintenanceMode());
         $this->addDeployCommand(new Command\Deploy\Magento2\SetupUpgrade());
