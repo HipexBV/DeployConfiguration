094c12ce52712b74da45cccba3ceddaef98a1b35
pieterhipex <58983096+pieterhipex@users.noreply.github.com>
2020-02-16 18:33:13 +0100

    Typo

--- templates/deploy.magento2.php
+++ templates/deploy.magento2.php
@@ -9,7 +9,7 @@ namespace HipexDeployConfiguration;
 /**
  * Start by setting up the configuration
  *
- * The magento 1 configuration contains some default configuration for shared folders / files and running installers
+ * The magento 2 configuration contains some default configuration for shared folders / files and running installers
  * @see Configuration\Magento2::initializeDefaultConfiguration
  */
 $configuration = new Configuration\Magento2('https://github.com/HipexBV/DeployConfiguration.git');
